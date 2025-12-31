7643c283cd0fda0f73080e36fd10f0a7ab5050ed
mibienpanjoe <epixprint00@gmail.com>
2025-12-31 22:26:58 +0000

    feat: implement full homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
+import {resumes} from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,11 +15,20 @@ export default function Home() {
         <Navbar/>
 
         <section className="main-section">
-            <div className="page-heading">
+            <div className="page-heading py-2">
                 <h1>Track Your Applications & Resume Ratings</h1>
                 <h2>Review your submissions and check AI-powered feedback</h2>
             </div>
+
+            {resumes.length> 0 && (
+                <div className="resumes-section">
+                    {resumes.map((resume)=>(
+                        <ResumeCard key={resume.id} resume={resume} />
+                    ))}
+                </div>
+            )}
+
         </section>
-        {}
+
     </main>;
 }
